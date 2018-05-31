50e258567c0247c2c9f05b8b495ae00a70260036
KaurSamuel <Kaursamuel.kannel@tptlive.com>
2018-05-31 08:33:40 +0300

    Initial

---
+++ Praktika töö/Source/Code/CorePlugin/YourCustomComponentType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Duality;
+
+namespace Praktika_töö
+{
+	public class YourCustomComponentType : Component
+	{
+		
+	}
+}
---
+++ Praktika töö/Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace Praktika_töö.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class Praktika_tööEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "Praktika_tööEditorPlugin"; }
+		}
+	}
+}
