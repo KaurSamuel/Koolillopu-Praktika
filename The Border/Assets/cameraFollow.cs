0c5570a3a83f5877deb3e836ee8b826a8e4bf3bf
Si1mT <33309403+Si1mT@users.noreply.github.com>
2018-06-04 13:56:40 +0300

    camera follows player + collider testing

---
+++ The Border/Assets/cameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour {
+
+    public Transform Player;
+    //public GameObject Player;       //Public variable to store a reference to the player game object
+
+
+    public Vector3 offset;         //Private variable to store the offset distance between the player and camera
+                                    
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - Player.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Player.transform.position+offset;
+    }
+}
