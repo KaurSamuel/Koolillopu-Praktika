f75beaa36cb12ddb10d35594524bb293c8d05093
KaurSamuel <33308949+KaurSamuel@users.noreply.github.com>
2018-06-04 12:03:34 +0300

    testing movement

---
+++ The Border/Assets/movment_player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment_player : MonoBehaviour {
+
+    private Rigidbody2D player_rb;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+        player_rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        player_rb.AddForce(movement * speed);
+    }
+}
