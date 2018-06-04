3e34f51422c786d327a2a431c3bda4c41cb8995b
KaurSamuel <33308949+KaurSamuel@users.noreply.github.com>
2018-06-04 20:17:21 +0300

    movement enchanted(not working)

--- The Border/Assets/movment_player.cs
+++ The Border/Assets/movment_player.cs
@@ -6,26 +6,26 @@ public class movment_player : MonoBehaviour {
 
     private Rigidbody2D player_rb;
     public float speed;
-    private bool grounded = false;
-   // public Transform ground;
+    public bool grounded = false;
+    public Transform Ground;
     [HideInInspector]public bool jump = false;
     [HideInInspector] public bool facingRight = true;
     public float jumpforce= 1000f;
     public float moveForce = 365f;
     public float maxSpeed = 5f;
-
+    Animator anim;
     // Use this for initialization
     void Start () {
         player_rb = GetComponent<Rigidbody2D>();
-        
+        anim = GetComponent<Animator>();
 	}
 
     // Update is called once per frame
     private void Update()
     {
-        //grounded = Physics2D.Linecast(transform.position, ground.position, 1 << LayerMask.NameToLayer("Ground"));
+        grounded = Physics2D.Linecast(transform.position, Ground.position, 0 << LayerMask.NameToLayer("Ground"));
 
-        if (Input.GetButtonDown("Jump"))
+        if (Input.GetButtonDown("Jump")&& grounded == true)
         {
             jump = true;
         }
@@ -34,7 +34,7 @@ public class movment_player : MonoBehaviour {
     void FixedUpdate () {
 
         float H = Input.GetAxis("Horizontal");
-
+        anim.SetFloat("speed", Mathf.Abs(H));
 
         if (H * player_rb.velocity.x < maxSpeed)
             player_rb.AddForce(Vector2.right * H * moveForce);
@@ -47,11 +47,16 @@ public class movment_player : MonoBehaviour {
         else if (H < 0 && facingRight)
             Flip();
 
-
+       // var vel = player_rb.velocity;
+       // if (vel.y < 0 )
+       // {
+       //     jumpforce = jumpforce - vel.y;
+       // }
         if (jump)
         {
-            player_rb.AddForce(new Vector2(0f, jumpforce));
+            player_rb.AddForce(new Vector2(0f, jumpforce ));
             jump = false;
+           // jumpforce = 1000f;
         }
 
 
